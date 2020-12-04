16cc9322fd5c118895b1bfd0b5e1ebf5971b6ff7
Choi-UngGyu <cdr982116@gmail.com>
2020-12-04 09:46:56 +0900

    ready to deploy 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from 'graphql-yoga';
 import resolvers from './graphql/resolvers';
 
 const server = new GraphQLServer({
-    typeDefs: "src/graphql/schema.graphql",
+    typeDefs: "graphql/schema.graphql",
     resolvers
 });
 
